26e9f4cffd7221e72037cb5f2567c2a56acef9be
gabbo07 <guion.gabriele@itisgrassi.edu.it>
2025-03-19 10:10:13 +0100

    g

---
+++ Persona.java
@@ -0,0 +1,16 @@
+public class Persona{
+    private int id;
+    private int pianoDestinazione;
+
+    public Persona(int id, int pianoDestinazione){
+        this.id = 100;
+        this.pianoDestinazione = new random;
+        
+    }
+
+    public void saliSuAscensore(Ascensore a){
+        if(id == pianoDestinazione && porteAperte = true){
+            
+        }
+    }
+}
